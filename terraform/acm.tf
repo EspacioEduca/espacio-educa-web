381aaf505fa06949002099e8928447b810834c53
Jorge Cróquer <65205090+JorgeCroquer@users.noreply.github.com>
2026-02-28 10:46:40 -0400

    Add Route 53 record for root domain with alias

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -3,6 +3,18 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
+resource "aws_route53_record" "root_domain" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_acm_certificate" "cert" {
   provider          = aws.us_east_1
   domain_name       = var.domain_name
